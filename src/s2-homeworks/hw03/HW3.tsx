d4249f7fda4b68cc4010eb134f69983f572e16c3
zelyankovas <vas.zelenko@gmail.com>
2024-07-12 00:53:59 +0300

    change project_sp2

--- src/s2-homeworks/hw03/HW3.tsx
+++ src/s2-homeworks/hw03/HW3.tsx
@@ -19,21 +19,27 @@ import GreetingContainer from './GreetingContainer'
 
 // types
 export type UserType = {
-    _id: any // need to fix any
-    name: any // need to fix any
+    _id: string // need to fix any
+    name: string // need to fix any
 }
 
-export const pureAddUserCallback = (name: any, setUsers: any, users: any) => { // need to fix any
-    const user = { // need to fix
+export const pureAddUserCallback = (name: string, setUsers: (users: UserType[]) => void, users: UserType[]) => { // need to
+    // fix any
+    const user : UserType = {
+        _id: v1(),
+        name: name
     }
     setUsers([...users, user])
+    console.log("use3", users)
 }
 
 const HW3 = () => {
-    const [users, setUsers] = useState<any>([]) // need to fix any
+    const [users, setUsers] = useState<UserType[]>([]) // need to fix any
 
-    const addUserCallback = (name: any) => { // need to fix any
+    const addUserCallback = (name: string) => { // need to fix any
+        console.log("use2")
         pureAddUserCallback(name, setUsers, users)
+
     }
 
     return (
