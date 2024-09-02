41208077498b4e6b577f950b69ee22c42bfbaa3b
zelyankovas <vas.zelenko@gmail.com>
2024-09-02 16:50:20 +0300

    change project_sp2

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,12 +7,16 @@ type ActionType =
 export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            return [...state].sort((a,b) => {
+                if (action.payload === 'up') {
+                    return a.name.localeCompare(b.name)
+                } else {
+                    return b.name.localeCompare(a.name)
+                }
+            }) // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter((u:UserType) => u.age >= 18) // need to fix
         }
         default:
             return state
